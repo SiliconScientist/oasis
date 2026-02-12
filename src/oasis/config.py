69a95e6c2eeb2dcdedc857218b7fe0921d6178a7
Avery <ahill15@tulane.edu>
2026-02-12 14:55:51 -0600

    Enhance IngestConfig to include optional catbench_folder and initialize paths in Config

--- src/oasis/config.py
+++ src/oasis/config.py
@@ -1,11 +1,12 @@
 from tomllib import load
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 from pathlib import Path
 
 
 class IngestConfig(BaseModel):
     source: Path
+    catbench_folder: Optional[Path] = None
 
 
 class MLIPInterpretersConfig(BaseModel):
@@ -34,8 +35,16 @@ class Config(BaseModel):
     ingest: IngestConfig
     mlip: MLIPConfig
 
+    def init_paths(self):
+        catbench_folder = (
+            self.ingest.source.parent / f"{self.ingest.source.name}_catbench"
+        )
+        self.ingest.catbench_folder = catbench_folder
+
 
 def get_config():
     with open("config.toml", "rb") as f:
         cfg_data = load(f)
-    return Config(**cfg_data)
+        cfg = Config(**cfg_data)
+        cfg.init_paths()
+    return cfg
